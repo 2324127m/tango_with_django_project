59421fb5a810d2052cca700c6f096f76cf0a0204
Mohammad Majid <2324127m@student.gla.ac.uk>
2019-01-22 20:03:54 +0000

    Finished Chapter 6 of TangoWithDjango

--- populate_rango.py
+++ populate_rango.py
@@ -10,27 +10,35 @@ def populate():
 	# A list of dictionaries containing the pages we want to add into Python category
 	python_pages = [
 		{"title": "Official Python Tutorial",
-		 "url": "http://docs.python.org/2/tutorial/"},
+		 "url": "http://docs.python.org/2/tutorial/",
+		 "views": 10},
 		{"title": "How to Think like a Computer Scientist",
-		 "url": "http://www.greenteapress.com/thinkpython/"},
+		 "url": "http://www.greenteapress.com/thinkpython/",
+		 "views": 18},
 		{"title": "Learn Python in 10 Minutes",
-		 "url": "http://www.korokithakis.net/tutorials/python"} ]
+		 "url": "http://www.korokithakis.net/tutorials/python",
+		 "views": 15} ]
 
 	# A list of dictionaries containing the pages we want to add into our Django category
 	django_pages = [
 		{"title": "Official Django Tutorial",
-		 "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+		 "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+		 "views": 28},
         {"title":"Django Rocks",
-         "url":"http://www.djangorocks.com/"},
+         "url":"http://www.djangorocks.com/",
+         "views": 7},
         {"title":"How to Tango with Django",
-         "url":"http://www.tangowithdjango.com/"} ]
+         "url":"http://www.tangowithdjango.com/",
+         "views": 17} ]
 
 	# A list of dictionaries containint the pages we want to add into our Other Frameworks category
 	other_pages = [
 		{"title":"Bottle",
-		"url": "http://bottlepy.org/docs/dev/"},
+		"url": "http://bottlepy.org/docs/dev/",
+		"views": 30},
      	{"title":"Flask",
-		"url":"http://flask.pocoo.org"} ]
+		"url":"http://flask.pocoo.org",
+		"views": 7} ]
 
 	# A dictionary of dictionaries for our categories, mapping Category name to the a list of pages
 	cats = {"Python" : {"pages": python_pages, "views": 128, "likes": 64},
@@ -42,7 +50,7 @@ def populate():
 	for cat, cat_data in cats.items():
 		c = add_cat(cat, cat_data["views"], cat_data["likes"])
 		for p in cat_data["pages"]:
-			add_page(c, p["title"], p["url"])
+			add_page(c, p["title"], p["url"], p["views"])
 
 	# Print out the categories we've added
 	for c in Category.objects.all():
@@ -65,3 +73,4 @@ def add_cat(name, views, likes):
 if __name__ == '__main__':
 	print("Starting Rango population script...")
 	populate()
+
--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,12 @@ from rango import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^about/', views.about, name='about')
+	url(r'^about/', views.about, name='about'),
+
+	# Parameter: category_name_slug. Can be accessed in our view (it takes a parameter)
+	# consists of a sequence of alphanumeric character and hyphens which are between the
+	# rango/category/id.../
+	# Sequence will be stored into parameter and passed to views.show_category()
+	# No match => 404 not found
+	url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category')
 ]
